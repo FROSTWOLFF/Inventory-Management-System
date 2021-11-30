403ed0015e2edac4ab98082f89a66a0064e78287
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-11-30 15:55:21 +0300

    Basic of the products is done NEXT => Refactoring and creating some new components(helpers.. etc) for a more maintainble structure

--- client/src/components/products/ProductTable/ProductTable.jsx
+++ client/src/components/products/ProductTable/ProductTable.jsx
@@ -4,19 +4,45 @@ import classes from './ProductTable.module.css';
 function ProductTable() {
   return (
     <table className={classes.table}>
-      <th className={classes.tableHead}>
-        <td>
-          <input type="checkbox" className={classes.checkbox} />
-        </td>
-        <td>Name</td>
-        <td>Id</td>
-        <td>Category</td>
-        <td>Location</td>
-        <td>Price</td>
-        <td>Added In</td>
-        <td>Modified</td>
-      </th>
-      <tbody className={classes.tableBody}>
+      <thead>
+        <tr>
+          <td>
+            <input type="checkbox" className={classes.checkbox} />
+          </td>
+          <td>Id</td>
+          <td>Name</td>
+          <td>Category</td>
+          <td>Location</td>
+          <td>Price</td>
+          <td>Added In</td>
+          <td>Modified</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+        </tr>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+        </tr>
         <tr>
           <td>
             <input type="checkbox" />
