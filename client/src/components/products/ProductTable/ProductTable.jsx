654580fe4ffff9d37ed03701e54c14ff86186623
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-11-30 15:19:40 +0300

    Reversing commit

--- client/src/components/products/ProductTable/ProductTable.jsx
+++ client/src/components/products/ProductTable/ProductTable.jsx
@@ -4,13 +4,32 @@ import classes from './ProductTable.module.css';
 function ProductTable() {
   return (
     <table className={classes.table}>
-      <th>
-        <td>Checkbox</td>
+      <th className={classes.tableHead}>
+        <td>
+          <input type="checkbox" className={classes.checkbox} />
+        </td>
+        <td>Name</td>
         <td>Id</td>
+        <td>Category</td>
         <td>Location</td>
         <td>Price</td>
+        <td>Added In</td>
         <td>Modified</td>
       </th>
+      <tbody className={classes.tableBody}>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+          <td>Dogus</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
