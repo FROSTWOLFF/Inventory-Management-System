1a710aa37eab8620b24fe629f3ee19d4404cc9c5
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-12-01 17:28:26 +0300

    Adjusted the new colors

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Filter.module.css';
 import FilterInput from './FilterInput.jsx';
 import FilterButton from './FilterButton.jsx';
+import FilterSearch from './FilterSearch';
 
 function Filter() {
   const options1 = ['Ali', 'Dogus', 'Temizsoy'];
@@ -12,6 +13,7 @@ function Filter() {
       <h1 className={classes.title}>Filter Products</h1>
       <form className={classes.form}>
         <div className={classes.inputs}>
+          <FilterSearch />
           <FilterInput title="Tags" placeholder="Filter By Tag" />
           <FilterInput title="Location" placeholder="All locations" />
           <FilterInput title="Category" placeholder="All categories" />
---
+++ client/src/components/filter/FilterSearch.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FilterSearch() {
+  return <div>filterSearch</div>;
+}
+
+export default FilterSearch;
