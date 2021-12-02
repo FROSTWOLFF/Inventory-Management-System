138b0c5076a01bb43424eb0e701094887b720fa3
Ali Dogus Temizsoy <dogustemizsoy@gmail.com>
2021-12-02 11:55:38 +0300

    Pagination and the styling of it is added as a component

--- client/src/components/products/Pagination/Pagination.jsx
+++ client/src/components/products/Pagination/Pagination.jsx
@@ -5,9 +5,9 @@ import PaginationButton from './PaginationButton.jsx';
 function Pagination() {
   return (
     <div className={classes.pagination}>
-      <PaginationButton label="First" />
-      <PaginationButton label="Previous" />
-      <PaginationButton label="1" />
+      <PaginationButton label="First" disabled />
+      <PaginationButton label="Previous" disabled />
+      <PaginationButton label="1" selected={true} />
       <PaginationButton label="2" />
       <PaginationButton label="3" />
       <PaginationButton label="4" />
--- client/src/components/products/Pagination/PaginationButton.jsx
+++ client/src/components/products/Pagination/PaginationButton.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import classes from './PaginationButton.module.css';
 
-function PaginationButton({ label }) {
-  return <button className={classes.button}>{label}</button>;
+function PaginationButton(props) {
+  const selectedClass = props.selected ? classes.selected : '';
+
+  return (
+    <button type={props.type} className={`${classes.button} ${selectedClass}`} {...props}>
+      {props.label}
+    </button>
+  );
 }
 
 export default PaginationButton;
